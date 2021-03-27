d5825f135884a4a732e7a1c8c3b6fc4d39ca91dc
Alex Berger <alexbigkid@users.noreply.github.com>
2021-03-26 20:46:20 -0700

    t tested function get_environment_variable_value

--- tests/test_env_loader.py
+++ tests/test_env_loader.py
@@ -1,7 +1,10 @@
 """Unit tests for env_loader.py"""
 
 # Standard library imports
+import os
+# from unittest import TestCase, mock
 import unittest
+from unittest.mock import patch
 
 # Local application imports
 from src.env_loader import EnvLoader
@@ -12,8 +15,21 @@ class TestEnvLoader(unittest.TestCase):
     def setUp(self):
         self.env_loader = EnvLoader()
 
-    def test_get_environment_variable_value(self):
-        self.assertTrue(True)
+    @patch.dict(os.environ, {'ABK_TEST_ENV_VAR': '[fake_api_key]'})
+    def test_get_environment_variable_value_returns_valid_value(self):
+        actual_value = self.env_loader.get_environment_variable_value('ABK_TEST_ENV_VAR')
+        self.assertEqual(actual_value, '[fake_api_key]')
+
+    @patch.dict(os.environ, {'ABK_TEST_ENV_VAR': ''})
+    def test_get_environment_variable_value_should_return_empty_given_env_var_value_is_empty(self):
+        actual_value = self.env_loader.get_environment_variable_value('ABK_TEST_ENV_VAR')
+        self.assertEqual(actual_value, '')
+
+    def test_get_environment_variable_value_should_return_empty_given_env_var_undefined(self):
+        actual_value = self.env_loader.get_environment_variable_value('ABK_TEST_ENV_VAR')
+        self.assertEqual(actual_value, '')
+
+
 
     def test_set_environment_varaibales_from_file(self):
         self.assertTrue(True)
